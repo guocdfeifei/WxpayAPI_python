50dca870245826ceb8c746024e1376bac0092bf3
xxx <xxx_828@163.com>
2018-05-01 23:05:01 +0800

    修改Api中的docstring

--- WxpayAPI_python/Api.py
+++ WxpayAPI_python/Api.py
@@ -18,7 +18,7 @@ class WxPayApi():
     """
     接口访问类，包含所有微信支付API列表的封装，类中方法为static方法，
     每个接口有默认超时时间（除提交被扫支付为10s，上报超时时间为1s外，其他均为6s）
-     """
+    """
 
     def unifiedOrder(self, inputObj, timeOut=6):
         """
@@ -64,8 +64,8 @@ class WxPayApi():
         """
         查询订单，WxPayOrderQuery中out_trade_no、transaction_id至少填一个
         appid、mchid、spbill_create_ip、nonce_str不需要填入
-        @param WxPayOrderQuery inputObj
-        @param int timeOut
+        @param inputObj WxPayOrderQuery
+        @param timeOut int
         @throws WxPayException
         @return 成功时返回，其他抛异常
         """
@@ -91,8 +91,8 @@ class WxPayApi():
         """
         关闭订单，WxPayCloseOrder中out_trade_no必填
         appid、mchid、spbill_create_ip、nonce_str不需要填入
-        @param WxPayCloseOrder inputObj
-        @param int timeOut
+        @param inputObj WxPayCloseOrder
+        @param timeOut int
         @throws WxPayException
         @return 成功时返回，其他抛异常
         """
@@ -113,13 +113,13 @@ class WxPayApi():
 
     def refund(self, inputObj, timeOut=6):
         """
-          申请退款，WxPayRefund中out_trade_no、transaction_id至少填一个且
-          out_refund_no、total_fee、refund_fee、op_user_id为必填参数
-          appid、mchid、spbill_create_ip、nonce_str不需要填入
-          @param WxPayRefund inputObj
-          @param int timeOut
-          @throws WxPayException
-          @return 成功时返回，其他抛异常
+        申请退款，WxPayRefund中out_trade_no、transaction_id至少填一个且
+        out_refund_no、total_fee、refund_fee、op_user_id为必填参数
+        appid、mchid、spbill_create_ip、nonce_str不需要填入
+        @param inputObj WxPayRefund
+        @param timeOut int
+        @throws WxPayException
+        @return 成功时返回，其他抛异常
         """
         url = "https://api.mch.weixin.qq.com/secapi/pay/refund"
         # 检测必填参数
@@ -144,16 +144,18 @@ class WxPayApi():
         self.reportCostTime(url, startTimeStamp, result)  # 上报请求花费时间
         return result
 
-    #  查询退款
-    #  提交退款申请后，通过调用该接口查询退款状态。退款有一定延时，
-    #  用零钱支付的退款20分钟内到账，银行卡支付的退款3个工作日后重新查询退款状态。
-    #  WxPayRefundQuery中out_refund_no、out_trade_no、transaction_id、refund_id四个参数必填一个
-    #  appid、mchid、spbill_create_ip、nonce_str不需要填入
-    #  @param WxPayRefundQuery inputObj
-    #  @param int timeOut
-    #  @throws WxPayException
-    #  @return 成功时返回，其他抛异常
     def refundQuery(self, inputObj, timeOut=6):
+        """
+        查询退款
+        提交退款申请后，通过调用该接口查询退款状态。退款有一定延时，
+        用零钱支付的退款20分钟内到账，银行卡支付的退款3个工作日后重新查询退款状态。
+        WxPayRefundQuery中out_refund_no、out_trade_no、transaction_id、refund_id四个参数必填一个
+        appid、mchid、spbill_create_ip、nonce_str不需要填入
+        @param inputObj WxPayRefundQuery
+        @param timeOut int
+        @throws WxPayException
+        @return 成功时返回，其他抛异常
+        """
         url = "https://api.mch.weixin.qq.com/pay/refundquery"
         # 检测必填参数
         if not inputObj.IsOut_refund_noSet() and \
@@ -172,13 +174,15 @@ class WxPayApi():
         self.reportCostTime(url, startTimeStamp, result)  # 上报请求花费时间
         return result
 
-    #  下载对账单，WxPayDownloadBill中bill_date为必填参数
-    #  appid、mchid、spbill_create_ip、nonce_str不需要填入
-    #  @param WxPayDownloadBill inputObj
-    #  @param int timeOut
-    #  @throws WxPayException
-    #  @return 成功时返回，其他抛异常
     def downloadBill(self, inputObj, timeOut=6):
+        """
+        下载对账单，WxPayDownloadBill中bill_date为必填参数
+        appid、mchid、spbill_create_ip、nonce_str不需要填入
+        @param inputObj WxPayDownloadBill
+        @param timeOut int
+        @throws WxPayException
+        @return 成功时返回，其他抛异常
+        """
         url = "https://api.mch.weixin.qq.com/pay/downloadbill"
         # 检测必填参数
         if not inputObj.IsBill_dateSet():
@@ -193,14 +197,16 @@ class WxPayApi():
             return ""
         return response
 
-    #  提交被扫支付API
-    #  收银员使用扫码设备读取微信用户刷卡授权码以后，二维码或条码信息传送至商户收银台，
-    #  由商户收银台或者商户后台调用该接口发起支付。
-    #  WxPayWxPayMicroPay中body、out_trade_no、total_fee、auth_code参数必填
-    #  appid、mchid、spbill_create_ip、nonce_str不需要填入
-    #  @param WxPayWxPayMicroPay inputObj
-    #  @param int timeOut
     def micropay(self, inputObj, timeOut=10):
+        """
+        提交被扫支付API
+        收银员使用扫码设备读取微信用户刷卡授权码以后，二维码或条码信息传送至商户收银台，
+        由商户收银台或者商户后台调用该接口发起支付。
+        WxPayWxPayMicroPay中body、out_trade_no、total_fee、auth_code参数必填
+        appid、mchid、spbill_create_ip、nonce_str不需要填入
+        @param inputObj WxPayWxPayMicroPay
+        @param timeOut int
+        """
         url = "https://api.mch.weixin.qq.com/pay/micropay"
         # 检测必填参数
         if not inputObj.IsBodySet():
@@ -223,12 +229,14 @@ class WxPayApi():
         self.reportCostTime(url, startTimeStamp, result)  # 上报请求花费时间
         return result
 
-    #  撤销订单API接口，WxPayReverse中参数out_trade_no和transaction_id必须填写一个
-    #  appid、mchid、spbill_create_ip、nonce_str不需要填入
-    #  @param WxPayReverse inputObj
-    #  @param int timeOut
-    #  @throws WxPayException
     def reverse(self, inputObj, timeOut=6):
+        """
+        撤销订单API接口，WxPayReverse中参数out_trade_no和transaction_id必须填写一个
+        appid、mchid、spbill_create_ip、nonce_str不需要填入
+        @param inputObj WxPayReverse
+        @param timeOut int
+        @throws WxPayException
+        """
         url = "https://api.mch.weixin.qq.com/secapi/pay/reverse"
         # 检测必填参数
         if not inputObj.IsOut_trade_noSet() and not inputObj.IsTransaction_idSet():
@@ -244,14 +252,17 @@ class WxPayApi():
         self.reportCostTime(url, startTimeStamp, result)  # 上报请求花费时间
         return result
 
-    #  测速上报，该方法内部封装在report中，使用时请注意异常流程
-    #  WxPayReport中interface_url、return_code、result_code、user_ip、execute_time_必填
-    #  appid、mchid、spbill_create_ip、nonce_str不需要填入
-    #  @param WxPayReport inputObj
-    #  @param int timeOut
-    #  @throws WxPayException
-    #  @return 成功时返回，其他抛异常
     def report(self, inputObj, timeOut=1):
+        """
+        测速上报，该方法内部封装在report中，使用时请注意异常流程
+        WxPayReport中interface_url、return_code、result_code、user_ip、execute_time_必填
+        appid、mchid、spbill_create_ip、nonce_str不需要填入
+        @param inputObj WxPayReport
+        @param timeOut int
+        @throws WxPayException
+        @return 成功时返回，其他抛异常
+        :return: 
+        """
         url = "https://api.mch.weixin.qq.com/payitil/report"
         # 检测必填参数
         if not inputObj.IsInterface_urlSet():
@@ -276,13 +287,15 @@ class WxPayApi():
         response = self.postXmlCurl(xml, url, False, timeOut)
         return response
 
-    #  生成二维码规则,模式一生成支付二维码
-    #  appid、mchid、spbill_create_ip、nonce_str不需要填入
-    #  @param WxPayBizPayUrl inputObj
-    #  @param int timeOut
-    #  @throws WxPayException
-    #  @return 成功时返回，其他抛异常
     def bizpayurl(self, inputObj, timeOut=6):
+        """
+        生成二维码规则,模式一生成支付二维码
+        appid、mchid、spbill_create_ip、nonce_str不需要填入
+        @param inputObj WxPayBizPayUrl
+        @param timeOut int
+        @throws WxPayException
+        @return 成功时返回，其他抛异常
+        """
         if not inputObj.IsProduct_idSet():
             raise WxPayException("生成二维码，缺少必填参数product_id！")
         inputObj.SetAppid(WxPayConfig.APPID)  # 公众账号ID
@@ -296,8 +309,8 @@ class WxPayApi():
         #  该接口主要用于扫码原生支付模式一中的二维码链接转成短链接(weixin:#wxpay/s/XXXXXX)，
         #  减小二维码数据量，提升扫描速度和精确度。
         #  appid、mchid、spbill_create_ip、nonce_str不需要填入
-        #  @param WxPayShortUrl inputObj
-        #  @param int timeOut
+        #  @param inputObj WxPayShortUrl
+        #  @param timeOut int
         #  @throws WxPayException
         #  @return 成功时返回，其他抛异常
 
@@ -317,12 +330,14 @@ class WxPayApi():
         self.reportCostTime(url, startTimeStamp, result)  # 上报请求花费时间
         return result
 
-    #  支付结果通用通知
-    #  @param def callback
-    #  直接回调函数使用方法: notify(you_def)
-    #  回调类成员函数方法:notify(array(this, you_def))
-    #  callback  原型为：def def_name(data)
     def notify(self, callback, msg):  # todo msg
+        """
+        支付结果通用通知
+        @param callback def
+        直接回调函数使用方法: notify(you_def)
+        回调类成员函数方法:notify(array(this, you_def))
+        callback  原型为：def def_name(data)
+        """
         # 获取通知的数据
         xml = file_get_contents('php://input')  # todo 应该需要传参
         # 如果返回成功则验证签名
@@ -334,7 +349,7 @@ class WxPayApi():
         return callback(result)
         #
         #  产生随机字符串，不长于32位
-        #  @param int length
+        #  @param length int
         #  @return 产生的随机字符串
 
     def getNonceStr(self, length=32):
@@ -344,17 +359,21 @@ class WxPayApi():
             str += substr(chars, mt_rand(0, strlen(chars) - 1), 1)
         return str
 
-    #  直接输出xml
-    #  @param string xml
     def replyNotify(self, xml):
+        """
+        直接输出xml
+        @param xml string
+        """
         return xml
         # echo xml  # todo 改为返回
 
-    #  上报数据， 上报的时候将屏蔽所有异常流程
-    #  @param string usrl
-    #  @param int startTimeStamp
-    #  @param array data
     def reportCostTime(self, url, startTimeStamp, data):
+        """
+        上报数据， 上报的时候将屏蔽所有异常流程
+        @param usrl string
+        @param startTimeStamp int
+        @param data array
+        """
         # 如果不需要上报数据
         if WxPayConfig.REPORT_LEVENL == 0:
             return
@@ -396,13 +415,15 @@ class WxPayApi():
         except WxPayException as e:
             pass  # 不做任何处理
 
-    #  以post方式提交xml到对应的接口url
-    #  @param string xml  需要post的xml数据
-    #  @param string url  url
-    #  @param bool useCert 是否需要证书，默认不需要
-    #  @param int second   url执行超时时间，默认30s
-    #  @throws WxPayException
     def postXmlCurl(self, xml, url, useCert=False, second=30):
+        """
+        以post方式提交xml到对应的接口url
+        @param string xml  需要post的xml数据
+        @param string url  url
+        @param bool useCert 是否需要证书，默认不需要
+        @param int second   url执行超时时间，默认30s
+        @throws WxPayException
+        """
         ch = curl_init()
         # 设置超时
         curl_setopt(ch, CURLOPT_TIMEOUT, second)
@@ -439,8 +460,11 @@ class WxPayApi():
             curl_close(ch)
             raise WxPayException("curl出错，错误码:" + error)
 
-    #  获取毫秒级别的时间戳
     def getMillisecond(self):
+        """
+        获取毫秒级别的时间戳
+        :return:
+        """
         # 获取毫秒的时间戳
         # 简化了 php 版本的逻辑
         return int(time.time() * 1000)
